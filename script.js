a3e88cd487c6ec11af2b8dba1e3ecf98016d6d06
DavidVaaa <64923176+DavidVaaa@users.noreply.github.com>
2023-05-20 22:05:00 -0300

    Implementacion de la Solucion

--- script.js
+++ script.js
@@ -1,48 +1,52 @@
-function addRecommendation() {
-    // Get the message of the new recommendation
-    let recommendation = document.getElementById("new_recommendation");
-    // If the user has left a recommendation, display a pop-up
-    if (recommendation.value != null && recommendation.value.trim() != "") {
-      console.log("New recommendation added");
-      showPopup(True);
+document.getElementById("rangeForm").addEventListener("submit", function(event) {
+  event.preventDefault();
   
-      // Create a new 'recommendation' element and set it's value to the user's message
-      var element = document.createElement("div");
-      element.setAttribute("class","recommendation");
-      element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
-      // Add this element to the end of the list of recommendations
-      document.getElementById("all_recommendations").appendChild(element); 
+  var minE = document.getElementById("minE").value;
+  var maxE = document.getElementById("maxE").value;
+  var minRo = document.getElementById("minRo").value;
+  var maxRo = document.getElementById("maxRo").value;
+  
+  var request = {
+    "E": [minE, maxE],
+    "Ro": [minRo, maxRo]
+  };
+  
+  fetch("URL_DE_TU_FUNCION_LAMBDA", {
+    method: "POST",
+    body: JSON.stringify(request)
+  })
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    displayResults(data);
+  })
+  .catch(function(error) {
+    console.log("Error:", error);
+  });
+});
+
+function displayResults(data) {
+  var materialList = document.getElementById("materialList");
+  materialList.innerHTML = ""; // Limpiar la lista de materiales anteriores
+  
+  if (data.statusCode === 200) {
+    var materials = JSON.parse(data.body);
+    
+    if (materials.length > 0) {
+      materials.forEach(function(material) {
+        var materialItem = document.createElement("div");
+        materialItem.textContent = material.Material;
+        materialList.appendChild(materialItem);
+      });
       
-      // Reset the value of the textarea
-      recommendation.value = "";
-    }
-  }
-  
-  function showPopup(bool) {
-    if (bool) {
-      document.getElementById('popup').style.visibility = 'visible'
+      document.getElementById("resultSection").style.display = "block";
     } else {
-      document.getElementById('popup').style.visibility = 'hidden'
+      materialList.textContent = "No se encontraron materiales que cumplan con los rangos especificados.";
+      document.getElementById("resultSection").style.display = "block";
     }
-  }
-  
-function toggleSidebar() {
-  var sidebar = document.querySelector('.sidebar');
-  var toggleButton = document.querySelector('.toggle-button');
-  const toggleIcon = document.querySelector('.toggle-icon');
-  const menuItems = document.querySelectorAll('.menu li a');
-
-  sidebar.classList.toggle('expanded');
-  toggleButton.classList.toggle('expanded');
-  toggleIcon.classList.toggle('expanded');
-
-  if (sidebar.classList.contains('expanded')) {
-    menuItems.forEach(item => {
-      item.style.paddingLeft = '10px';
-    });
   } else {
-    menuItems.forEach(item => {
-      item.style.paddingLeft = '20px';
-    });
+    materialList.textContent = "Error al obtener los materiales.";
+    document.getElementById("resultSection").style.display = "block";
   }
 }
